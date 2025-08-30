56439bcf24d4b1aa841a17972057602972b132d8
Jesse Shawl <jesse@jesse.sh>
2025-08-30 19:34:49 -0400

    scaffold

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Neoncrm_Calendar
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}

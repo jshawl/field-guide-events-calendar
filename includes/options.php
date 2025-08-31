1a748903138cf0eb365a801a8b3f64633eb7a85b
Jesse Shawl <jesse@jesse.sh>
2025-08-30 21:30:03 -0400

    add enqueue tests

--- includes/options.php
+++ includes/options.php
@@ -2,18 +2,14 @@
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Cached accessor for neoncrm_calendar options.
+ * accessor for neoncrm_calendar options.
  *
  * Usage:
  *   neoncrm_calendar_get_option( 'neoncrm_api_key', '' );
  *   neoncrm_calendar_get_option( null ); // returns full array
  */
 function neoncrm_calendar_get_option( $key = null, $default = '' ) {
-	static $opts = null;
-
-	if ( null === $opts ) {
-		$opts = (array) get_option( 'neoncrm_calendar_options', array() );
-	}
+	$opts = (array) get_option( 'neoncrm_calendar_options', array() );
 
 	if ( null === $key ) {
 		return $opts;

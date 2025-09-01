a336b2a2e6396be0316f2708ee7f28b9a0223f1c
Jesse Shawl <jesse@jesse.sh>
2025-09-01 13:54:35 -0400

    add sha to release zip (#13)

--- bin/zip.sh
+++ bin/zip.sh
@@ -2,8 +2,9 @@
 
 version=$(cat neoncrm-calendar.php | grep 'Version:' | sed 's/Version: //')
 slug=$(basename $(pwd))
+sha=$(git rev-parse --short HEAD)
 
-zip -r "release/$slug-$version.zip" \
+zip -r "release/$slug-$version-$sha.zip" \
     assets/ \
     includes/ \
     templates/ \

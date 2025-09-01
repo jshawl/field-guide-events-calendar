9592dde454a4ed3261cc3c875f9b975b3f1c9ed3
Jesse Shawl <jesse@jesse.sh>
2025-09-01 07:00:37 -0400

    add listEvents route

--- includes/rest.php
+++ includes/rest.php
@@ -2,6 +2,13 @@
 defined( 'ABSPATH' ) || exit;
 
 add_action( 'rest_api_init', function() {
+	register_rest_route( 'neoncrm-calendar/v1', '/listEvents', array(
+		'methods'  => 'GET',
+		'callback' => 'neoncrm_calendar_rest_list_events',
+	) );
+} );
+
+add_action( 'rest_api_init', function() {
 	register_rest_route( 'neoncrm-calendar/v1', '/events', array(
 		'methods'  => 'GET',
 		'callback' => 'neoncrm_calendar_rest_get_events',
@@ -43,6 +50,39 @@ function neoncrm_calendar_get_from_cache($cache_key, $url, $args) {
     return new WP_Error( 'neon_error', $decoded, array( 'status' => $code ) );
 }
 
+function neoncrm_calendar_rest_list_events( WP_REST_Request $request ) {
+    // todo before action validate keys in both routes
+	$api_key = neoncrm_calendar_get_option( 'neoncrm_api_key', '' );
+	$org_id  = neoncrm_calendar_get_option( 'neoncrm_org_id', '' );
+	if ( empty( $api_key ) ) {
+		return new WP_Error( 'no_api_key', 'API key not configured', array( 'status' => 500 ) );
+	}
+    if ( empty( $org_id ) ) {
+        return new WP_Error( 'no_org_id', 'Org ID not configured', array( 'status' => 500 ) );
+    }
+    $base = 'https://api.neoncrm.com/v2/events';
+    $args = array(
+        'headers' => array(
+            'Authorization' => 'Basic ' . base64_encode( $org_id . ':' . $api_key ),
+            'Content-Type' => 'application/json'
+        ),
+        'timeout' => 15
+    );
+    $start_date = gmdate('Y-m-d', strtotime('-1 month'));
+    $end_date = gmdate('Y-m-d', strtotime('+3 month'));
+    $params = array(
+        'startDateAfter=' . rawurlencode( $start_date ),
+        'endDateBefore=' . rawurlencode( $end_date ),
+        'pageSize=200',
+    );
+    $neon_events_url = $base . '?' . implode( '&', $params );
+    $events = neoncrm_calendar_get_from_cache('listEvents', $neon_events_url, $args);
+    if ( empty ($events) ) {
+        return new WP_Error( 'no_events', 'Could not get events from Neon CRM', array( 'status' => 500 ) );
+    }
+    return rest_ensure_response($events);
+}
+
 function neoncrm_calendar_rest_get_events( WP_REST_Request $request ) {
     // todo before action validate keys in both routes
 	$api_key = neoncrm_calendar_get_option( 'neoncrm_api_key', '' );
@@ -55,7 +95,6 @@ function neoncrm_calendar_rest_get_events( WP_REST_Request $request ) {
     }
     $start_date = gmdate('Y-m-d', strtotime('-1 month'));
     $end_date = gmdate('Y-m-d', strtotime('+3 month'));
-    $base = 'https://api.neoncrm.com/v2/events/search/outputFields';
     $data = [
         'searchFields' => [
             [

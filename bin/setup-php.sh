496b07e11fe9eb4fb4e01a9d3e824e4430d104ac
Jesse Shawl <jesse@jesse.sh>
2025-09-01 21:29:22 -0400

    improve setup scripts (#16)

---
+++ bin/setup-php.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+npm run wp-env run tests-cli bash -- -c 'cd wp-content/plugins/neon-crm-calendar && composer install'
+npm run wp-env run tests-cli -- sudo pecl install pcov
+
+npm run wp-env run tests-cli bash -- -c 'echo "extension=pcov" | sudo tee /usr/local/etc/php/conf.d/99-pcov.ini > /dev/null'
+npm run wp-env run tests-cli bash -- -c 'echo "pcov.enabled=1" | sudo tee -a /usr/local/etc/php/conf.d/99-pcov.ini > /dev/null'

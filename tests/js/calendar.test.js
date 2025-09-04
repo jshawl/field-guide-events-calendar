ff64afeb4bcec8683ae7c29f4a5c635b0b7b4fb2
Jesse Shawl <jesse@jesse.sh>
2025-09-04 05:27:12 -0400

    add test coverage

--- tests/js/calendar.test.js
+++ tests/js/calendar.test.js
@@ -6,6 +6,7 @@ import {
   formatEvents,
   getCampaignNames,
   getEvents,
+  main,
   render,
   renderCalendar,
   renderCampaigns,
@@ -14,6 +15,49 @@ import {
 vi.stubGlobal("campaign_calendar", {
   org_id: "abcd",
   rest_url: "/fake-url",
+});
+
+const addEventMock = vi.fn();
+const calendarRenderMock = vi.fn();
+
+vi.stubGlobal("FullCalendar", {
+  Calendar: class {
+    constructor(_element, opts) {
+      onEventClick = opts.eventClick;
+    }
+    addEvent(args) {
+      return addEventMock(args);
+    }
+    getEvents() {
+      return [];
+    }
+    render() {
+      return calendarRenderMock();
+    }
+  },
+});
+
+describe("main", () => {
+  beforeEach(() => {
+    document.body.innerHTML = `<div class="campaign_calendar">
+      <div class="loading"></div>
+      <div class="calendar"></div>
+    </div>`;
+  });
+
+  it("renders the calendar", async () => {
+    const mockResponse = {
+      events: [{ name: "Sample Event" }],
+    };
+    globalThis.fetch = vi.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockResponse),
+      }),
+    );
+    await main();
+    expect(calendarRenderMock).toHaveBeenCalledTimes(1);
+    expect(document.querySelector(".campaign_calendar .loading")).toBeNull();
+  });
 });
 
 describe("formatEvents", () => {
@@ -40,6 +84,7 @@ describe("formatEvents", () => {
       title: "Sample Event",
     });
   });
+
   it("uses the start date and end time if multi_day_events is false", () => {
     const events = [
       {
@@ -77,6 +122,7 @@ describe("render", () => {
     render({ calendar, campaignName: "All", events: [] });
     expect(document.querySelector(".campaign_calendar .loading")).toBeNull();
   });
+
   it("removes existing calendar events", () => {
     const removeMock = vi.fn();
     const calendar = {
@@ -85,6 +131,7 @@ describe("render", () => {
     render({ calendar, campaignName: "All", events: [] });
     expect(removeMock).toHaveBeenCalled();
   });
+
   it("adds events matching the campaign filter", () => {
     const addMock = vi.fn();
     const calendar = {
@@ -103,27 +150,10 @@ describe("render", () => {
 
 describe("renderCampaigns", () => {
   let options = {};
+  let calendar = {};
+  const addMock = vi.fn();
   beforeEach(() => {
     options.filter_campaigns = "true";
-  });
-  it("returns early if filtering is disabled", () => {
-    options.filter_campaigns = "false";
-    const container = document.createElement("div");
-    const calendar = { getEvents: vi.fn().mockReturnValue([]) };
-    const events = [];
-    const result = renderCampaigns({ calendar, container, events, options });
-    expect(result).toBeUndefined();
-  });
-  it("adds buttons for each campgaign", () => {
-    const container = document.createElement("div");
-    const calendar = { getEvents: vi.fn().mockReturnValue([]) };
-    const events = [{ campaignName: "Workshop" }, { campaignName: "Seminar" }];
-    renderCampaigns({ calendar, container, events, options });
-    expect(container.querySelectorAll("input[type='radio']").length).toBe(3); // Including "All"
-  });
-  it("renders calendar on campaign change", () => {
-    const container = document.createElement("div");
-    const addMock = vi.fn();
     const events = [
       { campaignName: "Workshop" },
       { campaignName: "Seminar" },
@@ -132,12 +162,21 @@ describe("renderCampaigns", () => {
       addEvent: addMock,
       remove: vi.fn(),
     }));
-    const calendar = {
+    calendar = {
       addEvent: addMock,
       getEvents: vi.fn().mockReturnValue(events),
     };
+    document.body.innerHTML = "<div class='container'></div>";
+    const container = document.querySelector(".container");
     renderCampaigns({ calendar, container, events, options });
-    const seminarRadio = container.querySelector("input[value='Seminar']");
+  });
+
+  it("adds buttons for each campgaign", () => {
+    expect(document.querySelectorAll("input[type='radio']").length).toBe(3); // Including "All"
+  });
+
+  it("renders calendar on campaign change", () => {
+    const seminarRadio = document.querySelector("input[value='Seminar']");
     seminarRadio.checked = true;
     seminarRadio.dispatchEvent(new Event("change", { bubbles: true }));
     expect(addMock).toHaveBeenCalledTimes(1);
@@ -145,20 +184,21 @@ describe("renderCampaigns", () => {
       expect.objectContaining({ campaignName: "Seminar" }),
     );
   });
+
+  it("returns early if filtering is disabled", () => {
+    options.filter_campaigns = "false";
+    const container = document.createElement("div");
+    const calendar = { getEvents: vi.fn().mockReturnValue([]) };
+    const events = [];
+    renderCampaigns({ calendar, container, events, options });
+    expect(container.querySelectorAll("input[type='radio']").length).toBe(0);
+  });
 });
 
 let onEventClick = () => {};
 describe("renderCalendar", () => {
   it("initializes the calendar", () => {
     vi.stubGlobal("open", vi.fn());
-    vi.stubGlobal("FullCalendar", {
-      Calendar: class {
-        constructor(_element, opts) {
-          onEventClick = opts.eventClick;
-        }
-        render() {}
-      },
-    });
     const calendarEl = document.createElement("div");
     renderCalendar(calendarEl);
     onEventClick({ event: { id: "1" } });

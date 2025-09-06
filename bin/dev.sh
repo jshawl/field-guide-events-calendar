94ea8a96dd2575fd4db694f443f7b7b5d95f6af2
Jesse Shawl <jesse@jesse.sh>
2025-09-06 12:11:55 -0400

    dont reinstall pcov

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
 npm run wp-env run tests-cli bash -- -c 'cd wp-content/plugins/field-guide-events-calendar && composer install' || exit 1
+
+npm run wp-env run tests-cli -- php -i | grep 'pcov' > /dev/null && exit 0
 npm run wp-env run tests-cli -- sudo pecl install pcov
 
 npm run wp-env run tests-cli bash -- -c 'echo "extension=pcov" | sudo tee /usr/local/etc/php/conf.d/99-pcov.ini > /dev/null'

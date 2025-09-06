3ce19fae0f803a84cb1cb5b3367082a5b64812f2
Jesse Shawl <jesse@jesse.sh>
2025-09-06 10:45:15 -0400

    fix dev/ci

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npm run wp-env run tests-cli bash -- -c 'cd wp-content/plugins/field_guide_events_calendar && composer install'
+npm run wp-env run tests-cli bash -- -c 'cd wp-content/plugins/field-guide-events-calendar && composer install' || exit 1
 npm run wp-env run tests-cli -- sudo pecl install pcov
 
 npm run wp-env run tests-cli bash -- -c 'echo "extension=pcov" | sudo tee /usr/local/etc/php/conf.d/99-pcov.ini > /dev/null'

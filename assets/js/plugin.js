a8a6bac99e2c8513bb2b42aed24df1ea8f7f250b
Jesse Shawl <jesse@jesse.sh>
2025-09-06 20:31:58 -0400

    fix lint

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -10,8 +10,8 @@ let currentModel = { ...initialModel };
 
 export const init = () => {
   const container = document.querySelector(".field_guide_events_calendar");
-  dispatch({ type: "OPTIONS_SET", options: container.dataset });
-  dispatch({ type: "INIT_CALENDAR", el: container.querySelector("#calendar") });
+  dispatch({ options: container.dataset, type: "OPTIONS_SET" });
+  dispatch({ el: container.querySelector("#calendar"), type: "INIT_CALENDAR" });
 };
 
 const dispatch = (action) => {
@@ -27,16 +27,15 @@ const dispatch = (action) => {
 };
 
 const commands = {
-  initializeCalendar: () => {},
   fetchEvents: async ({ start, end, dispatch }) => {
     const url = new URL(`${field_guide_events_calendar.rest_url}/neon/events`);
     url.searchParams.append("start", start);
     url.searchParams.append("end", end);
     const response = await fetch(url.toString());
     const { events } = await response.json();
-    dispatch({ type: "EVENTS_FETCHED", events });
+    dispatch({ events, type: "EVENTS_FETCHED" });
   },
-  onEventClick: async ({ id }) => {
+  onEventClick: ({ id }) => {
     const url = `https://${field_guide_events_calendar.org_id}.app.neoncrm.com/np/clients/${field_guide_events_calendar.org_id}/event.jsp?event=${id}`;
     window.open(url, "_blank");
   },
@@ -44,49 +43,45 @@ const commands = {
 
 const update = (msg, model) => {
   switch (msg.type) {
-    case "OPTIONS_SET":
+    case "OPTIONS_SET": {
       return { ...model, options: msg.options };
-    case "INIT_CALENDAR":
-      const calendar = new FullCalendar.Calendar(msg.el, {
-        datesSet: async (info) => dispatch({ type: "DATES_SET", info }),
-        eventClassNames: ["field_guide_events_calendar-event"],
-        eventClick: (info) =>
-          dispatch({ type: "ON_EVENT_CLICK", id: info.event.id }),
-        headerToolbar: {
-          left: "title",
-          right: "prev,next today",
-        },
-        height: "auto",
-        initialView: "dayGridMonth",
-      });
+    }
+
+    case "INIT_CALENDAR": {
+      const calendar = new FullCalendar.Calendar(msg.el, calendarOptions);
       calendar.render();
       return { ...model, calendar };
+    }
 
-    case "DATES_SET":
+    case "DATES_SET": {
       const start = msg.info.startStr.slice(0, 10);
       const end = msg.info.endStr.slice(0, 10);
-      return [{ ...model, loading: true, start, end }, commands.fetchEvents];
+      return [{ ...model, end, loading: true, start }, commands.fetchEvents];
+    }
 
-    case "EVENTS_FETCHED":
+    case "EVENTS_FETCHED": {
       const events = formatEvents({
         events: msg.events,
         options: model.options,
       });
-      if (getCampaignNames(events).indexOf(model.filter) === -1) {
+      if (getCampaignNames(events).includes(model.filter)) {
         model.filter = "All";
       }
-      return { ...model, loading: false, events };
+      return { ...model, events, loading: false };
+    }
 
-    case "CAMPAIGN_FILTER_CHANGED":
+    case "CAMPAIGN_FILTER_CHANGED": {
       const { filter } = msg;
       return { ...model, filter };
+    }
 
-    case "ON_EVENT_CLICK":
+    case "ON_EVENT_CLICK": {
       return [model, () => commands.onEventClick({ id: msg.id })];
+    }
 
-    default:
-      console.warn("unhandled msg:", msg);
-      return model;
+    default: {
+      throw new Error(`Unhandled message: ${JSON.stringify(msg)}`);
+    }
   }
 };
 
@@ -94,7 +89,12 @@ const view = (model, dispatch) => {
   model.calendar?.removeAllEvents();
   model.events
     .filter((event) => ["All", event.campaignName].includes(model.filter))
-    .map((event) => model.calendar.addEvent(event));
+    .map((event) =>
+      model.calendar.addEvent({
+        ...event,
+        allDay: event.startDate !== event.endDate,
+      }),
+    );
   if (model.loading) {
     model.loadingEl.style.display = "block";
   } else {
@@ -121,8 +121,8 @@ const view = (model, dispatch) => {
               ${campaignName}
             </label>
           `;
-    div.addEventListener("change", (e) =>
-      dispatch({ type: "CAMPAIGN_FILTER_CHANGED", filter: e.target.value }),
+    div.addEventListener("change", (event) =>
+      dispatch({ filter: event.target.value, type: "CAMPAIGN_FILTER_CHANGED" }),
     );
     container.append(div);
   });
@@ -130,6 +130,18 @@ const view = (model, dispatch) => {
 
 // Helpers
 // -------
+
+const calendarOptions = {
+  datesSet: (info) => dispatch({ info, type: "DATES_SET" }),
+  eventClassNames: ["field_guide_events_calendar-event"],
+  eventClick: (info) => dispatch({ id: info.event.id, type: "ON_EVENT_CLICK" }),
+  headerToolbar: {
+    left: "title",
+    right: "prev,next today",
+  },
+  height: "auto",
+  initialView: "dayGridMonth",
+};
 
 export const getCampaignNames = (events) => [
   "All",

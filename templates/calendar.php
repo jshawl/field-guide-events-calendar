cbd1b93e55e74c51eec2488c2ef83ce4b05e5413
Jesse Shawl <jesse@jesse.sh>
2025-09-07 19:26:34 -0400

    standardize class names

--- templates/calendar.php
+++ templates/calendar.php
@@ -1,10 +1,10 @@
 <?php defined("ABSPATH") || exit(); ?>
-<div class="field_guide_events_calendar"
+<div class="field_guide_events_calendar_container"
 	<?php foreach ($atts as $key => $value): ?>
 		data-<?php echo esc_attr($key); ?>="<?php echo esc_attr($value); ?>"
 	<?php endforeach; ?>
 >
-	<div class="loading"></div>
-	<div class="campaigns"></div>
-	<div id="calendar"></div>
+	<div class="field_guide_events_calendar_loading"></div>
+	<div class="field_guide_events_calendar_campaigns"></div>
+	<div class="field_guide_events_calendar_calendar"></div>
 </div>

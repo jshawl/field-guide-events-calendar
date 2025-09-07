dd5b36c4322ff84ef65a7b66d94cbc102e97311a
Jesse Shawl <jesse@jesse.sh>
2025-09-06 20:04:06 -0400

    wip

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -38,10 +38,29 @@ const commands = {
 
 const update = (msg, model) => {
   switch (msg.type) {
+    case "OPTIONS_SET":
+      return { ...model, options: msg.options };
+    case "INIT_CALENDAR":
+      const calendar = new FullCalendar.Calendar(msg.el, {
+        datesSet: async (info) => dispatch({ type: "DATES_SET", info }),
+        eventClassNames: ["field_guide_events_calendar-event"],
+        eventClick: (info) =>
+          dispatch({ type: "ON_EVENT_CLICK", id: info.event.id }),
+        headerToolbar: {
+          left: "title",
+          right: "prev,next today",
+        },
+        height: "auto",
+        initialView: "dayGridMonth",
+      });
+      calendar.render();
+      return { ...model, calendar };
+
     case "DATES_SET":
       const start = msg.info.startStr.slice(0, 10);
       const end = msg.info.endStr.slice(0, 10);
       return [{ ...model, loading: true, start, end }, commands.fetchEvents];
+
     case "EVENTS_FETCHED":
       const events = formatEvents({
         events: msg.events,
@@ -51,12 +70,16 @@ const update = (msg, model) => {
         model.filter = "All";
       }
       return { ...model, loading: false, events };
+
     case "CAMPAIGN_FILTER_CHANGED":
       const { filter } = msg;
       return { ...model, filter };
+
     case "ON_EVENT_CLICK":
       return [model, () => commands.onEventClick({ id: msg.id })];
+
     default:
+      console.warn("unhandled msg:", msg);
       return model;
   }
 };
@@ -71,50 +94,39 @@ const view = (model, dispatch) => {
   } else {
     model.loadingEl.style.display = "none";
   }
-  const campaignNames = getCampaignNames(model.events);
-  const container = document.querySelector(
-    ".field_guide_events_calendar .campaigns",
-  );
-  container.innerHTML = "";
-  campaignNames.forEach((campaignName) => {
-    const div = document.createElement("div");
-    let checkedAttribute = "";
-    if (campaignName === model.filter) {
-      checkedAttribute = "checked='true'";
-    }
-    div.innerHTML = `
-        <input id="field_guide_events_calendar_campaign_name_${campaignName}" type="radio" name="field_guide_events_calendar_campaign_name" value="${campaignName}" ${checkedAttribute}/>
-        <label for="field_guide_events_calendar_campaign_name_${campaignName}">
-          ${campaignName}
-        </label>
-      `;
-    div.addEventListener("change", (e) => {
-      const filter = e.target.value;
-      dispatch({ type: "CAMPAIGN_FILTER_CHANGED", filter });
+  if (model.options.filter_campaigns === "true") {
+    const campaignNames = getCampaignNames(model.events);
+    const container = document.querySelector(
+      ".field_guide_events_calendar .campaigns",
+    );
+    container.innerHTML = "";
+    campaignNames.forEach((campaignName) => {
+      const div = document.createElement("div");
+      let checkedAttribute = "";
+      if (campaignName === model.filter) {
+        checkedAttribute = "checked='true'";
+      }
+      div.innerHTML = `
+            <input id="field_guide_events_calendar_campaign_name_${campaignName}" type="radio" name="field_guide_events_calendar_campaign_name" value="${campaignName}" ${checkedAttribute}/>
+            <label for="field_guide_events_calendar_campaign_name_${campaignName}">
+              ${campaignName}
+            </label>
+          `;
+      div.addEventListener("change", (e) => {
+        const filter = e.target.value;
+        dispatch({ type: "CAMPAIGN_FILTER_CHANGED", filter });
+      });
+      container.append(div);
     });
-    container.append(div);
-  });
+  }
 };
 
 export const init = () => {
   const container = document.querySelector(".field_guide_events_calendar");
-  const calendarEl = container.querySelector("#calendar");
-  const calendar = new FullCalendar.Calendar(calendarEl, {
-    datesSet: async (info) => dispatch({ type: "DATES_SET", info }),
-    eventClassNames: ["field_guide_events_calendar-event"],
-    eventClick: (info) =>
-      dispatch({ type: "ON_EVENT_CLICK", id: info.event.id }),
-    headerToolbar: {
-      left: "title",
-      right: "prev,next today",
-    },
-    height: "auto",
-    initialView: "dayGridMonth",
-  });
-  calendar.render();
   const options = container.dataset;
-  currentModel = { ...currentModel, calendar, options };
-  view(currentModel, dispatch);
+  dispatch({ type: "OPTIONS_SET", options });
+  const calendarEl = container.querySelector("#calendar");
+  dispatch({ type: "INIT_CALENDAR", el: calendarEl });
 };
 
 // Helpers
